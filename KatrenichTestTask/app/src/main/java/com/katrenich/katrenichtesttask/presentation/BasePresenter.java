2f1cbe2a6602d815915e623b6e15332cf5cb5bfb
AlexKatrenich <alex.katrenich@gmail.com>
2019-05-04 21:20:39 +0300

    Зверстано ActionBar та додано Основний презентер, Клас для зберігання посилань на презентери

---
+++ KatrenichTestTask/app/src/main/java/com/katrenich/katrenichtesttask/presentation/BasePresenter.java
@@ -0,0 +1,48 @@
+package com.katrenich.katrenichtesttask.presentation;
+
+import android.support.annotation.NonNull;
+import java.lang.ref.WeakReference;
+
+public abstract class BasePresenter<M, V>{
+    protected M model;
+    private WeakReference<V> view;
+
+    /* отримання та фіксація посилання на модель(об'єкт, список об'єктів), яку повинна відображати View*/
+    public void setModel(M model) {
+        this.model = model;
+        if (setupDone()){
+            updateView();
+        }
+    }
+
+    /* метод для фіксації полсилання(WeakReference) на View, перевірка актуальності View/Model та перевідображення даних на екрані*/
+    public void bindView(@NonNull V view){
+        this.view = new WeakReference<>(view);
+        if(setupDone()){
+            updateView();
+        }
+    }
+
+    /* метод для відкріплення посилання на View*/
+    public void unbindView(){
+        this.view = null;
+    }
+
+    /* метод для отримання посилання на View якщо вона не знищена GarbageCollector*/
+    protected V view() {
+        if (view == null) {
+            return null;
+        } else {
+            return view.get();
+        }
+    }
+
+    /* метод для перевірки актуальності посилань на View(Екран) та модель*/
+    protected boolean setupDone() {
+        return view() != null && model != null ;
+    }
+
+    /* бізнес-логіка в презентері, наприклад виклик методів для "пере-відображення" списку об'єктів на View*/
+    protected abstract void updateView();
+
+}
